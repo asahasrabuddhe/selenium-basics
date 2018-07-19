522fb9c16cd3610795a7633e1e60c491546906dc
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2018-07-19 18:36:22 +0530

    Refactored code to support helper functions

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 
+import static com.perennial.selenium.ScreenshotHelper.captureScreenshot;
+
 
 public class FirstTest  {
     public static void main(String[] args) {
@@ -35,32 +37,5 @@ public class FirstTest  {
 
         //Close the browser
         driver.quit();
-    }
-
-    public static void captureScreenshot(WebDriver driver, String destinationPath) {
-        // Initialize Javascript Executor to allow us to execute JS code on the webpage.
-        JavascriptExecutor je = (JavascriptExecutor)driver;
-        // Return the actual height of the page using JS
-        Long height = (Long)je.executeScript("return document.body.scrollHeight;");
-        // Backup the existing dimension to reset before function exist
-        Dimension oldDimension = driver.manage().window().getSize();
-        // Set new dimension = full height x 1280
-        Dimension d = new Dimension(1280, height.intValue());
-        driver.manage().window().setSize(d);
-
-        // Initialize Screenshot Driver
-        TakesScreenshot ts = (TakesScreenshot)driver;
-        // Take a screenshot and store as a file
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        // Create destination file
-        File destination = new File(destinationPath);
-        // Copy file to destination
-        try {
-            FileUtils.copyFile(source, destination);
-        } catch( Exception e) {
-            System.out.println(e.getMessage());
-        }
-        // Reset dimension
-        driver.manage().window().setSize(oldDimension);
     }
 }
